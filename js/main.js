e61e9303c5207391b84c46cc9d7f9db4ca2a9285
luvnonsence <136680329+luvnonsence@users.noreply.github.com>
2023-09-27 17:49:52 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,5 +4,28 @@ $(function () {
 		$('.header__nav-contacts').toggleClass('header__nav-contacts--active');
 	});
 
-	// 123
+	// Модальное окно
+	$('.btn--open').click(function () {
+		$(body).addClass('body--fixed');
+		$('.modal').fadeIn(300);
+		return false;
+	});
+	$('.modal__close-btn').click(function () {
+		$(body).removeClass('body--fixed');
+		$(this).parents('.modal').fadeOut(300);
+		return false;
+	});
+	$(document).keydown(function (e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$(body).removeClass('body--fixed');
+			$('.modal').fadeOut(300);
+		}
+	});
+	$('.modal').click(function (e) {
+		if ($(e.target).closest('.modal__window').length == 0) {
+			$(body).removeClass('body--fixed');
+			$(this).fadeOut(300);
+		}
+	});
 });
